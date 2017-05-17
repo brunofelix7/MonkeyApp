b9d643631806e8ef53d2c3df20ceaa682c17d7d9
Bruno <bruno@synctech.com.br>
2017-05-17 09:06:54 -0300

    aula command

--- MonkeyApp/MonkeyApp/ViewModel/MainViewModel.cs
+++ MonkeyApp/MonkeyApp/ViewModel/MainViewModel.cs
@@ -5,12 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace MonkeyApp.ViewModel {
 
     public class MainViewModel : BaseViewModel{
 
         private string _descricao;
+        private string _searchTerm;
+        
+        //  Classe qua implementa um ICommand, usa no construtor
+        //  Vai ser sempre ReadOnly, você nunca vai alterar a instância dessa variável
+        public Command SearchCommand { get; }
+
+        public string SearchTerm {
+            get { return _searchTerm; }
+            set { SetProperty(ref _searchTerm, value); }
+        }
+
 
         public string Descricao {
             get { return _descricao; }
@@ -42,6 +55,22 @@ namespace MonkeyApp.ViewModel {
                 //  O nameOf evita que eu coloque entre parênteses o nome da variável e venha a errar esse nome
                 //  PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Descricao)));
             });
+
+            //  O segundo construtor é opcional, retorna um bool, se for true o botão pode executar a ação, se for false o botão é desabilitado
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+
         }
+
+        //  Executa qualquer coisa que eu quiser ao clicar o botão
+        void ExecuteSearchCommand() {
+            Debug.WriteLine($"Fui clicado em {DateTime.Now}");
+        }
+
+        //  Você pode acrescentar alguma lógica para ativar ou não o clique do botão
+        bool CanExecuteSearchCommand() {
+            return true;
+        }
+
     }
+
 }
